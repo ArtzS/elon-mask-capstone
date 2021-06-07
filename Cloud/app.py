655f222a036809fb22238ed6f4bb7fae64c7b47c
ArtzS <79263166+ArtzS@users.noreply.github.com>
2021-06-07 16:10:11 +0700

    Python flask API [UNUSED]
    Python file for flask app (API). Invokes the machine learning model in compute engine, and stores the resulting video to gs://elon-mask-video-bucket as an mp4 video file.

---
+++ Cloud/app.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, request, send_from_directory, jsonify
+import os
+import pickle
+from google.cloud import storage
+
+model = pickle.load(open("/home/c0050503/Mask Detector YOLOV4/model.pkl", 'rb'))
+
+client = storage.Client()
+bucket = client.get_bucket('elon-mask-video-bucket')
+
+UPLOAD_FOLDER = "/home/c0050503/Mask Detector YOLOV4/video test"
+MY_IP = os.system('curl ifconfig.me')
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+@app.route("/", methods=['GET','POST'])
+def home():
+	user_input = request.form.get('video')
+	os.chdir('/home/c0050503/Mask Detector YOLOV4/detection')
+	os.system('python /home/c0050503/Mask\ Detector\ YOLOV4/detection/detect_video.py --weights /home/c0050503/Mask\ Detector\ YOLOV4/detection/TFmodel --size 416 --model yolov4 --video /home/c0050503/Mask\ Detector\ YOLOV4/video\ test/Wearing\ a\ face\ mask\ in\ scared\ of\ coronavirus\ in\ the\ Philippines.mp4 --output /home/c0050503/Mask\ Detector\ YOLOV4/result.mp4 --count')
+
+#	blob = bucket.blob('result.mp4')
+#	blob.upload_from_filename('result.mp4')
+
+	return render_template('index.html')
+
+
+
+
+if __name__ == "__main__":
+	app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8081)), debug=True)
+
+	os.chdir('..')
+	blob = bucket.blob('result.mp4')
+	blob.upload_from_filename('result.mp4')
