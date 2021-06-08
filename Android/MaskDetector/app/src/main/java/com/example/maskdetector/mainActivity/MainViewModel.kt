c4f8b2229e1e081e87ea0a02a499a341c3887f25
KensenH <56247114+KensenH@users.noreply.github.com>
2021-06-09 01:40:09 +0700

    Delete Android/MaskDetector directory

--- Android/MaskDetector/app/src/main/java/com/example/maskdetector/mainActivity/MainViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.example.maskdetector.mainActivity
-
-import androidx.lifecycle.ViewModel
-import com.example.cleanarchitecture.core.domain.usecase.AppUseCase
-
-
-class MainViewModel(private val appUseCase : AppUseCase) : ViewModel() {
-
-}
